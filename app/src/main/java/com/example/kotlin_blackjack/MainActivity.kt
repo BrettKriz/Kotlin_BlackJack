731577f34454f643263d2d913e70ed0d2e3a9de5
Brett - PERSONAL <brettkriz@gmail.com>
2020-10-26 23:00:01 -0400

    Companions Test

--- app/src/main/java/com/example/kotlin_blackjack/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_blackjack/MainActivity.kt
@@ -3,14 +3,17 @@ package com.example.kotlin_blackjack
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Switch
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-    var ET_MinBet:EditText? = null
 
-    var TV_CashM: TextView? = null
+    var ET_MinBet:EditText? = null
+    var TV_CashM:TextView? = null
+    var TOG_UseCash:Switch? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,23 +21,39 @@ class MainActivity : AppCompatActivity() {
         // Start Code Area
         ET_MinBet  = findViewById<EditText>(R.id.et_MinBet)
         TV_CashM = findViewById<TextView>(R.id.tv_Cash_Menu)
+        TOG_UseCash = findViewById<Switch>(R.id.tog_UseCash)
 
         updateCash()
     }
 
     fun updateCash(){
-        TV_CashM!!.setText("$" + TheTable::Cash)
+        TV_CashM!!.setText("$ " + TheTable.SHARED.Cash)
     }
 
-    fun clickReset(){
+    fun clickReset(view: View){
         println("[i] Reseting money to Minbet")
-        TheTable::resetCash.call()
+
+        // Reset money
+        TheTable.SHARED.Cash = ( TheTable.SHARED.CashScaler * TheTable.SHARED.MinBet )
+
         updateCash()
+        updateSHARED()
     }
 
-    fun clickGotoGame(){
+    fun updateSHARED(){
+        println("[+] Updating Main SHARED data")
+
+        TheTable.SHARED.UseCash = TOG_UseCash!!.isChecked()
+        TheTable.SHARED.MinBet = ET_MinBet!!.text.toString().toInt() ?: 100
+
+    }
+
+    fun clickGotoGame(view: View){
         // Change activity
         println("[i] Entering the Table")
+
+        updateSHARED()
+
         updateCash()
         startActivity(Intent(this, TheTable::class.java))
     }
--- app/src/main/java/com/example/kotlin_blackjack/TheTable.kt
+++ app/src/main/java/com/example/kotlin_blackjack/TheTable.kt
@@ -6,16 +6,17 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
-import android.widget.Switch
 import android.widget.TextView
 
 class TheTable : AppCompatActivity() {
-    var isInitd = false
+    var isInitd = false // Make it easy to tell if we've started yet
+
+    // Define finals
     val GAME_SCORE_CEIL = 10 // Dont let value be too much
 
-    public var Cash = 0 // The table uses money the most, therefore base it in here
-    val CashScaler = 4
-    var isBetDoubled = false
+    //var Cash = 400 // The table uses money the most, therefore base it in here
+    //val CashScaler = 4
+    var BetDoubled = false
 
     // Track static elements
     var B_Hit: Button? = null
@@ -31,24 +32,41 @@ class TheTable : AppCompatActivity() {
     var TV_Name: TextView? = null
 
     var ET_CurBet: EditText? = null
-    var ET_MinBet: EditText? = null
-    var ET_Name: EditText? = null
+    //var ET_MinBet: EditText? = null
+    //var ET_Name: EditText? = null
 
-    var TOG_UseCash: Switch? = null
+    //var TOG_UseCash: Switch? = null
 
+    // Create resource tables
     val AllSuits = listOf("Diamonds", "Clubs", "Hearts", "Spades")
     val AllSuitsC = listOf('♦', '♣', '♥', '♠')
     val AllFaces = listOf("Ace", "Jack", "Queen", "King", "Joker")
     val AllColors = listOf("Red" , "Black")
 
+    // Define a custom type for cards
     class Card(val index:Int, val value:Int, val Suit:Int, var Holder:Boolean?) // Null = deck, true = player, false = dealer
 
     var Deck: Array<Card?> = arrayOfNulls<Card>(52)
+    var Hand_Player: Array<Card?> = arrayOfNulls<Card>(13)
+    var Hand_Dealer: Array<Card?> =  arrayOfNulls<Card>(13)
 
-    var Hand_Player: Array<Card?> = arrayOfNulls<Card>(52)
-    var Hand_Dealer: Array<Card?> =  arrayOfNulls<Card>(52)
+    // Add a companion object to store shared vars
+    companion object SHARED {
+        // Forget reflection
+        // COMPANIONS ARE GODLIKE!
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+        // vars
+        var Cash = 400
+        var MinBet = 100
+        var UseCash = true
+        var PName = "Player"
+        val CashScaler = 4
+
+        // funcs
+    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_the_table)
         // Start Code Area
@@ -58,13 +76,19 @@ class TheTable : AppCompatActivity() {
 
             createDeck()
             initTable()
+            // Start the game with reset
             resetGame()
         }
     }
 
     fun createDeck(){
+        // Create all 13 values of cards
+        // and create the suits as well
+        println("[i] Creating the Deck!")
+
         for (x in 0 until 13){
-            // Make 4 at a time
+            // Make all 4
+
             val i1 = 0 + x
             val i2 = 1 + x
             val i3 = 2 + x
@@ -75,36 +99,13 @@ class TheTable : AppCompatActivity() {
             Deck[i2] = Card(i2, x+1, 1, null)
             Deck[i3] = Card(i3, x+1, 2, null)
             Deck[i4] = Card(i4, x+1, 3, null)
-
         }
     }
-    fun getSuitName(s: Int):String{ // Might not use, too long
-        return AllSuits[s]
-    }
-    fun getSuitChar(s: Int):Char{
-        return AllSuitsC[s]
-    }
-    fun getCardName(v: Int):String{ // Value, not index
-        var ans = v.toString()
-        val arg = v // Casting relic
 
-        if (arg == 1 ) {
-            // Search for alt
-            ans = AllFaces[0]
-        }else if (arg > 10) {
-            ans = AllFaces[arg - 10]
-        }
-
-        return ans
-    }
-    fun getCardColor(s: Int):String{
-        val arg = s % 2
-        return AllColors[arg]
-    }
-    // val button = findViewById<Button>(R.id.Button)
-    //fun sendMessage(view: View) {
     fun initTable(){
         // Reset everything
+        println("[i] Building element vars")
+
         B_Hit = findViewById<Button>(R.id.b_GHit)
         B_Double = findViewById<Button>(R.id.b_GDouble)
         B_Stay = findViewById<Button>(R.id.b_GStay)
@@ -118,24 +119,20 @@ class TheTable : AppCompatActivity() {
         TV_Name = findViewById<TextView>(R.id.tv_Name)
 
         ET_CurBet  = findViewById<EditText>(R.id.et_CurBet)
-        ET_MinBet  = findViewById<EditText>(R.id.et_MinBet)
-        ET_Name = findViewById<EditText>(R.id.et_PlayerName)
+        //ET_MinBet  = findViewById<EditText>(R.id.et_MinBet)
+        //ET_Name = findViewById<EditText>(R.id.et_PlayerName)
 
-        TOG_UseCash = findViewById<Switch>(R.id.tog_UseCash)
+        //TOG_UseCash = findViewById<Switch>(R.id.tog_UseCash)
 
         // Correct logic
-        val cash_base = ET_MinBet!!.text.toString().toInt() ?: 100
-        setCash(CashScaler * cash_base)
-
-
+        resetCash()
     }
 
-    fun useCash():Boolean{
-        return TOG_UseCash!!.isSelected
-    }
-
-    fun setCash(amt: Int):Int {
+    fun setCash(amt: Int):Int{
+        // Money needs to be exchanged
+        // The game is over
         println("[i] Setting cash to $amt")
+
         val isWin = amt > 0
 
         if (useCash()) {
@@ -147,27 +144,39 @@ class TheTable : AppCompatActivity() {
                 Cash += amt
             }
 
-            TV_Cash!!.setText("$" + amt)
+            TV_Cash!!.setText("$ " + amt)
             //TV_CashM!!.setText(amt)
-
         }
 
-        endState(isWin, amt)
+        endState(isWin, amt) // Round over
 
         return amt
     }
 
     fun endState(isWin:Boolean, amt:Int){
+        // A hang state which unlocks the central text
+        println("Hiding buttons, unhiding text with onClick")
+
         TV_Main!!.visibility = View.VISIBLE
         var mstr = "Tie Game"
 
         if (amt > 0) {
             if (isWin) {
-                mstr = "You've won $" + amt
+                mstr = "You've won $ " + amt
             } else {
-                mstr = "You've lost $" + amt
+                var sadd = ""
+                if (isBust(true)){
+                    sadd = "Bust!"
+                }
+                mstr = sadd + "You've lost $ " + amt
             }
         }
+
+        // Let them know the game will end
+        if (isTooBroke()){
+            mstr = "You're broke!"
+        }
+
         TV_Main!!.setText(mstr)
 
         B_Double!!.visibility = View.INVISIBLE
@@ -176,8 +185,9 @@ class TheTable : AppCompatActivity() {
     }
 
     fun checkGameStatus(DMove: Boolean){
-        // Make sure nobodys won
-        println("Evaluate game")
+        // Someone has won, who is it?
+        println("[i] Evaluating game")
+
         var pt: Int = getPlayerTotal()
         var dt: Int = getDealerTotal()
 
@@ -186,159 +196,77 @@ class TheTable : AppCompatActivity() {
 
         if (DMove && dr && dt < 16){
             // Draw a card if dealer is below 16
+            // and give it to the dealer
             drawCardFromDeck(false)
 
             dt = getDealerTotal()
             dr = dt <= 21
         }
 
-        // Show dealers hand
+        // Show the dealers full hand
         TV_CDealer!!.setText(getDealerAll())
 
         if ((!pr && !dr) || (pr && dr && pt == dt)) {
             // Tie
-            Game_Tie()
+            game_Tie()
             return
-        }
-
-        // Busts
-        if (!pr){
+        }else if (!pr){ // Check Busts, then amounts
             // Bust Player
             // Lose
-            Game_Loose()
+            game_Lose()
 
         }else if (!dr){
             // Bust Dealer
             // Win
-            Game_Win()
+            game_Win()
 
         }else if (dr && dt > pt){
             // Player Loss
-            Game_Loose()
+            game_Lose()
         }else if (pr && pt > dt){
             // PLayer wins
-            Game_Win()
+            game_Win()
         }
 
     }
 
-    fun clickMain(view: View){
-        // Reset for another round
-        // Or boot player
-        resetGame()
+    fun drawCardFromDeck(side: Boolean):Card{
+        // Take a number from the deck at random
+        // and give it a side of the table
 
-        if (isTooBroke()) {
-            println("Too poor, exiting")
-            doGotoMain()
-        }
-    }
+        var go = true
+        var temp: Card
 
-    fun getPlayerTotal(): Int{
-        var total = 0
-        var str:String = "None"
+        // Get a random card thats not in a hand
+        do {
+            temp = Deck.random()!!
+        } while (temp.Holder != null)
 
-        for (x in 0 until Hand_Player.size){
-            val cur: Card = Hand_Player[x]!!
+        // Mark as held
+        temp.Holder = side
 
-            if (str.length < 1){
-                str = cardToStr(cur)
-            }else{
-                str = str + " , " + cardToStr(cur)
-            }
-
-            total = total + Math.max( cur.value, GAME_SCORE_CEIL )
+        // Add the card to the Hand
+        if (side) {
+            Hand_Player.plus(temp)
+            // Update card string
+            TV_CPlayer!!.setText(cardToStr(temp))
+        } else {
+            Hand_Dealer.plus(temp)
         }
 
-        TV_CPlayer!!.setText(str)
-
-        return total
+        return temp
     }
 
-    fun resetCash(){
-        val t:String = ET_MinBet!!.text.toString()
-        var amt = 100 * CashScaler
-
-        if (t.length > 0){
-            amt = t.toInt() * CashScaler
-        }
-
-        Cash = amt
-    }
-
-    fun getTotalBet(): Int {
-        var b = ET_CurBet.toString()
-        var m = ET_MinBet.toString()
-        var bi:Int = 100
-        var s:Int = 1
-
-
-        if (isBetDoubled){
-            s = 2
-        }
-        if (b.length > 0){
-            bi = b.toInt()
-        }
-        if (m.length > 0){
-            bi = Math.max(bi, m.toInt())
-        }
-
-        val ans:Int = (s * bi)
-        return ans
-    }
-
-    fun getDealerTotal(): Int{
-        var total = 0
-        for (x in 0 until Hand_Dealer.size){
-            val cur: Card = Hand_Dealer[x]!!
-            total = total + Math.max( cur.value, GAME_SCORE_CEIL )
-        }
-        return total
-    }
-
-    fun getDealerAll():  String{
-        var str:String = "None"
-
-        for (x in 0 until Hand_Dealer.size){
-            val cur: Card = Hand_Dealer[x]!!
-
-            if (str.length < 1) {
-                str = cardToStr(cur)
-            } else {
-                str = str + " , " + cardToStr(cur)
-            }
-        }
-
-        return str
-    }
-
-
-
-    fun Game_Win(){
-        val amt = setCash(getTotalBet() * 2)
-    }
-    fun Game_Loose(){
-        val amt = setCash(-1 * getTotalBet())
-
-        if (isTooBroke()) {
-            // Eject them from the game
-            doTooBroke()
-            return
-        }
-        // Keep going
-
-
-    }
-    fun Game_Tie(){
-        println("Tie game")
-        endState(false, 0)
-    }
     fun resetGame(){
-        TV_Name!!.setText( ET_Name!!.text )
+        // Rest sate to a start
 
-        isBetDoubled = false
+        val name_s = SHARED.PName
+        TV_Name!!.setText( name_s )
 
-        Hand_Dealer = arrayOfNulls<Card>(52)
-        Hand_Player = arrayOfNulls<Card>(52)
+        BetDoubled = false
+
+        Hand_Dealer = arrayOfNulls<Card>(13)
+        Hand_Player = arrayOfNulls<Card>(13)
 
         for (x in 0 until Deck.size){
             // Clean deck
@@ -370,34 +298,128 @@ class TheTable : AppCompatActivity() {
         TV_Main!!.visibility = View.INVISIBLE
     }
 
-    fun getMinBet():Int{
-        var arg = ET_MinBet!!.text.toString()
-        var ans: Int = 0
-        if (arg.length < 1) {
-            ans = 100
-            ET_MinBet!!.setText("100")
-        }else{
-            ans = arg.toInt() ?: 100
+    fun game_Win(){
+        // Add money and change status
+        println("[i] Win game")
+
+        val amt = setCash(getTotalBet() * 2)
+    }
+
+    fun game_Lose(){
+        // Subtract money and change status
+        println("[i] Lose game")
+
+        val amt = setCash(-1 * getTotalBet())
+    }
+
+    fun game_Tie(){
+        println("[i] Tie game")
+        endState(false, 0)
+    }
+
+    fun getPlayerTotal():Int{
+        // Sum the cards
+
+        var total = 0
+        var str:String = "None"
+
+        for (x in 0 until Hand_Player.size){
+            val cur: Card = Hand_Player[x]!!
+
+            if (str.length < 1){
+                str = cardToStr(cur)
+            }else{
+                str = str + " , " + cardToStr(cur)
+            }
+
+            total = total + Math.max( cur.value, GAME_SCORE_CEIL )
         }
 
+        TV_CPlayer!!.setText(str)
+
+        return total
+    }
+
+    fun resetCash(){
+        // Reset cash
+
+        //val t:String = ET_MinBet!!.text.toString()
+        val t:Int = 100
+        var amt = 100 * CashScaler
+
+        if (t != null){
+            amt = t * CashScaler
+        }else{
+            amt = 100
+        }
+
+        Cash = amt
+    }
+
+    fun getTotalBet():Int{
+        // Calculate the bet in full
+
+        var b = ET_CurBet.toString()
+        var m = SHARED.MinBet
+        var bi:Int = 100
+        var s:Int = 1
+
+        if (BetDoubled){
+            s = 2
+        }
+        if (b.length > 0){
+            bi = b.toInt()
+        }
+        if (m != null) {
+            bi = Math.max(bi, m)
+        } else {
+            bi = 100
+            println("[!] Shared bet min came back null! 0,0")
+        }
+
+        val ans:Int = (s * bi)
         return ans
     }
 
-    fun cardToStr(t:Card):String{
-        var ans:String = ""
+    fun getDealerTotal():Int{
+        // Calculate dealers cards
+        var total = 0
 
-        ans = ans + getSuitChar(t.Suit) + getCardName(t.value)
+        for (x in 0 until Hand_Dealer.size){
+            val cur: Card = Hand_Dealer[x]!!
+            total = total + Math.max( cur.value, GAME_SCORE_CEIL )
+        }
+
+        return total
+    }
+
+    fun getDealerAll():String{
+        // Get the string for ALL of the dealers cards
+        var str:String = "None"
+
+        for (x in 0 until Hand_Dealer.size){
+            val cur: Card = Hand_Dealer[x]!!
+
+            if (str.length < 1) {
+                str = cardToStr(cur)
+            } else {
+                str = str + " , " + cardToStr(cur)
+            }
+        }
+
+        return str
+    }
+
+    fun getMinBet():Int{
+        // Grab the current value
+        // for Min Bet
+
+        var arg = MinBet
+        var ans: Int = 0
+
+        ans = arg // Removed alot of test code here
 
         return ans
-    }
-
-    fun isTooBroke():Boolean{
-        return TOG_UseCash!!.isChecked && Cash < getMinBet()
-    }
-
-    fun doTooBroke(){
-        println("Too broke exit")
-        doGotoMain()
     }
 
     fun isBust(isP:Boolean):Boolean{
@@ -405,12 +427,82 @@ class TheTable : AppCompatActivity() {
         var amt: Int
 
         if (isP) {
+            // Is the player bust?
             amt = getPlayerTotal()
-        }else {
+        } else {
+            // Is the dealer bust?
             amt = getDealerTotal()
         }
 
         return amt > 21
+    }
+
+    fun isTooBroke():Boolean{
+        // Is player too broke?
+        val b1:Boolean = SHARED.UseCash
+        return b1 && Cash < getMinBet()
+    }
+
+    fun doTooBroke(){
+        println("[i] Too broke, exiting..")
+        doGotoMain()
+    }
+
+    fun useCash():Boolean{
+        // Return the value of the slider
+        return UseCash
+    }
+
+    fun getSuitName(s: Int):String{
+        // Might not use, too long
+        // see: Diamond Queen
+        return AllSuits[s]
+    }
+
+    fun getSuitChar(s: Int):Char{
+        return AllSuitsC[s]
+    }
+
+    fun getCardName(v: Int):String{
+        // Value, not index
+        // Returns the name of the card
+        var ans = v.toString()
+        val arg = v // Casting relic
+
+        if (arg == 1 ) {
+            // Search for alt
+            ans = AllFaces[0]
+        }else if (arg > 10) {
+            ans = AllFaces[arg - 10]
+        }
+
+        return ans
+    }
+
+    fun getCardColor(s: Int):String{
+        // Get colors of card
+        // @@Todo Colored cards
+        val arg = s % 2
+        return AllColors[arg]
+    }
+
+    fun cardToStr(t:Card):String{
+        // Build a sudo card
+        var ans:String = "["
+
+        ans = ans + getSuitChar(t.Suit) + getCardName(t.value) + "]"
+
+        return ans
+    }
+
+    fun clickMain(view: View){
+        // Reset for another round
+        // Or boot player
+        resetGame()
+
+        if (isTooBroke()) {
+            doTooBroke()
+        }
     }
 
     fun clickHit(view: View){
@@ -419,34 +511,18 @@ class TheTable : AppCompatActivity() {
 
     }
     fun clickDouble(view: View){
+        // Double the bet
         doDouble()
 
     }
     fun clickStay(view: View){
+        // Stay and end dealing
         doStay(true)
 
     }
     fun clickGotoMain(view: View){
+        // Exit to the main menu
         doGotoMain()
-    }
-
-    fun drawCardFromDeck(side: Boolean):Card{
-        var go = true
-        var temp: Card
-        do {
-            temp = Deck.random()!!
-        } while (temp.Holder != null)
-        // Mark as held
-        temp.Holder = side
-
-        if (side) {
-            Hand_Player.plus(temp)
-            TV_CPlayer!!.setText(cardToStr(temp))
-        } else {
-            Hand_Dealer.plus(temp)
-        }
-
-        return temp
     }
 
     fun doHit(side: Boolean){
@@ -456,20 +532,23 @@ class TheTable : AppCompatActivity() {
             checkGameStatus(false)
         }
     }
+
     fun doStay(side:Boolean){
         // Trigger the ending
 
         checkGameStatus(isBust(false))
     }
+
     fun doDouble(){
         // Dealer cant bet
-        isBetDoubled = true
+        BetDoubled = true
         B_Double!!.visibility = View.INVISIBLE
 
     }
+
     fun doGotoMain(){
         // Change activity
-        TV_CashM!!.setText("$" + Cash)
+        TV_CashM!!.setText("$ " + Cash)
         startActivity(Intent(this, MainActivity::class.java))
     }
 }
